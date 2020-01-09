78e9c00b729ae88b84caf30cba1741e46f9ca87f
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-01-09 05:54:48 +0100

    cambios en la vista Home

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 
+import router from "@router";
+
 Vue.config.productionTip = false;
 
 new Vue({
+  cuando el nombre de la propiedad es igual al nombre de la variable
+  router,
   render: h => h(App)
 }).$mount("#app");
