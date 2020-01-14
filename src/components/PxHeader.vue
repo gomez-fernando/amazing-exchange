6c64df63c580a952af5297b1d728477686607016
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-01-14 01:42:45 +0100

    ajuste márgenes del navabar

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -3,7 +3,7 @@
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <px-icon class="mr-2" />
+          <px-icon class="mr-2 ml-20" />
           <router-link
             :to="{ name: 'home' }"
             class="font-semibold text-xl tracking-tight"
